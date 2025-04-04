9e9de8511bd6018167772f57f2de83accb1b019b
JhoelDiego2 <jhoel.mita@sptech.school>
2025-04-04 00:03:40 -0300

    agregando a modelagem v1 e pasta arduino

--- Arduino/dht11_Pi/dht11_Pi.ino
+++ Arduino/dht11_Pi/dht11_Pi.ino
@@ -1,12 +1,3 @@
-/*INTEGRANTES
-  João Vitor
-  Henrique Dourado
-  Edson Félix
-  Leonardo Monteiro
-  Lorenzo Meni
-  Felipe Queiroz
-  Omar Nidal
-*/
 
 #include "DHT.h" // adiciona a biblioteca DHT.h ao código
 
@@ -14,7 +5,6 @@
 const int PINO_SENSOR_DHT11 = A4; // define em qual pino o sensor está ligado
 
 DHT sensorDHT(PINO_SENSOR_DHT11, TIPO_SENSOR); // cria um objeto do sensor DHT11
-
 
 void setup() {
   Serial.begin(9600); // define a velocidade de transmissão
